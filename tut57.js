a9390122971400f141f63046403a66019ea70366
Yash <pariharyash1989@gmail.com>
2022-01-07 22:48:19 +0530

    added eventlistener in tut57.js

--- tut57.js
+++ tut57.js
@@ -1,3 +1,14 @@
+let para = document.getElementById('para');
+para.addEventListener('mouseover', function run(){
+    // alert("mouse inside")
+    console.log("mouse inside")
+})
+
+para.addEventListener('mouseout', function run(){
+    // alert("mouse outside")
+    console.log("mouse outside")
+})
+
 function togglehide(){
     let btn = document.getElementById('btn');
     let para = document.getElementById('para');
